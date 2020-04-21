9dafad30fa765d24b471c7aa0857bdeb280bd555
MacLovin <alexisbrunet34@gmail.com>
2020-04-21 23:03:45 +0200

    route pour envoyer tous les articles liés aux sujets passé en path parameters

---
+++ controller/gNewsController.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const init = require('../init/init');
+
+
+module.exports = {
+
+    /**
+     * renvois les news en rapport avec les topic passé en paramètre 
+     */
+    getArticlesByTopic: async (topic) => {
+
+        let params = {
+            params: {
+                q: topic,
+                apiKey: init.gnewsApi.key
+            }
+        }
+        let gNewsEndPoint = init.gnewsApi.everythingEndPoint;
+
+        try {
+            const response = await axios.get(gNewsEndPoint, params);
+            return response.data.articles;
+
+        } catch (error) {
+            console.log("erreur lors de la requête à l'API Gnews : " + error);
+            return error;
+        }
+        
+    }
+}
--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const init = require('../init/init.js');
 const database = require('../controller/databaseController');
-const axios = require('axios');
+const gnews = require('../controller/gNewsController');
 
 let routes = init.routes;
 
@@ -10,29 +10,24 @@ router.post(init.routes.hello, (req, res) => {
     res.send("hello from private")
 });
 
+/**
+ * renvois tous les articles enregistré par un utilisateur
+ */
 router.get(routes.user + "/:userId" + routes.articles, async (req, res) => {
    //va cherche l'user avec son id;
     let userFounded = await database.getUserById(req.params.userId);
     if(userFounded){
 
         //pour chaque topic du user, interroge gnews
-        let userTopics = userFounded.topics;
-        userTopics.forEach(async topic => {
+        let articles = [];
 
-            let params = {
-                params: {
-                    q: topic,
-                    apiKey: init.gnewsApi.key
-                }
-            }
-            let gNewsEndPoint = init.gnewsApi.topHeadLineEndPoint;
-            axios.get(gNewsEndPoint, params).then(
-                res => console.log(res),
-                err => console.log(err)
-            )
+        for (let i = 0; i < userFounded.topics.length; i++) {
+            const topic = userFounded.topics[i];
+            let article = await gnews.getArticlesByTopic(topic);
+            articles.push(article);
+        }
 
-        });
-        //renvois les news trouvés par google
+        res.status(200).send(articles);
 
     }else{
         res.status(404).send({message: init.message.database.userNotFound})
