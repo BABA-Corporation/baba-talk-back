73ce037f89d5d82133556848cc6d265c1146ff9b
unknown <alexisbrunet34@gmail.com>
2020-06-12 17:17:33 +0200

    suppression body-parser (déjà inclu dans express)

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const init = require('./init/init');
 const router = require('./routes/router');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const express = require('express');
@@ -9,9 +8,6 @@ const app =  express();
 const figlet = require('figlet');
 
 app.use(init.routes.apiDoc, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(express.static('public'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use("/",cors(), router);
 
